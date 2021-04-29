26ff40f6284ca31f45d322e6a52c63ef7c2c8c6b
myOmikron <accounts@omikron.dev>
2021-04-29 17:30:17 +0200

    Added validate option and added it and all api endpoints to urls

--- validator/validator/urls.py
+++ validator/validator/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import api.urls
+from validator.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('internal/validate', Validate.as_view()),
+    path("api/v1/", include(api.urls)),
 ]
